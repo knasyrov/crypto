1b0e8e6f5c1734f48726003139bb75da6c4f1211
Kirill Nasyrov <knasyrov@gmail.com>
2025-04-30 09:47:09 +0500

    updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def number_to_sat(value)
+    number_to_currency(value.to_i, delimiter: ' ', precision: 0, unit: '') 
+  end
+
+  def format_date(date)
+    date.strftime("%d.%m.%Y %H:%M")
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,13 @@
 class Transaction < ApplicationRecord
   # belongs_to :in_addr, class_name: 'Address', foreign_key: :eid
   # belongs_to :change_addr, class_name: 'Address', foreign_key: :eid
-  #
-
+  
   validates :in_addr, :out_addr, :change_addr, presence: true
 
-  def human_state
-    [ "draft", "completed", "error" ][self.state.to_i]
-  end
+  enum state: {
+    draft: 0,
+    completed: 1,
+    error: 2
+  }
+
 end
